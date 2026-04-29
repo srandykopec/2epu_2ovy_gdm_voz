49ffc67d27b93cd017d69981a63370021fda4b16
Jozef Dubec <jozef.vyucba@gmail.com>
2026-04-29 12:13:00 +0200

    premenne a vypis

---
+++ OVY/09_js_premene_operatory/script.js
@@ -0,0 +1,50 @@
+/*
+ //===============================
+//1. cvicenie zmena hodnoty v premennej
+//======================
+
+
+let body = 0;
+
+console.log("Počet bodov:", body);      //1. alternativa vypisu
+console.log("Počet bodov: " + body );   //2. alternativa vypisu
+console.log(`Počet bodov: ${body} `);   //3. alternativa vypisu
+
+body = 5;
+console.log(`Počet bodov: ${body} `);
+body = 10;
+console.log(`Počet bodov: ${body} `);
+body = 25;
+console.log(`Počet bodov: ${body} `);
+body = 50;
+console.log(`Počet bodov: ${body} `);
+
+*/
+// Počet bodov: 0
+// Počet bodov: 10
+// Počet bodov: 25
+// Počet bodov: 50
+
+//==================================================
+/*
+const cislo1 = 10;
+const cislo2 = 3;
+const cislo3 = 5;
+
+
+
+console.log("10 + 5 = 15");    //údajový typ reťazec (string)
+
+console.log(`Spolu ${cislo1 + cislo2}`)
+console.log(`${cislo1} + ${cislo2} = ${cislo1 + cislo2}`)
+console.log(`${cislo1} - ${cislo2} = ${cislo1 - cislo2}`)
+console.log(`${cislo1} * ${cislo2} = ${cislo1 * cislo2}`)
+console.log(`${cislo1} / ${cislo2} = ${cislo1 / cislo2}`)
+console.log(`${cislo1} % ${cislo2} = ${cislo1 % cislo2}`)
+
+console.log(`==============================`);
+
+console.log(`(${cislo1} + ${cislo2}) * ${cislo3}  = ${(cislo1 + cislo2) * cislo3}`)
+*/
+
+//==================================================
