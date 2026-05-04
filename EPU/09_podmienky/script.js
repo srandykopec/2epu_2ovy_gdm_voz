469a97ff85ea5a8f6851c5273dcf4a4a7f51ec82
Jozef Dubec <127235676+srandykopec@users.noreply.github.com>
2026-05-04 13:54:24 +0200

    .

--- EPU/09_podmienky/script.js
+++ EPU/09_podmienky/script.js
@@ -11,9 +11,29 @@
 // Obvod a obsah kruhu
 //
 
-let polomer = 4;
-let pi = 3.14;
+// let polomer = 4;
+// let pi = 3.14;
 
-console.log(` Obsah kruhu s polomerom  ${polomer}  je: ${pi*(polomer^2)}`);
+// console.log(` Obsah kruhu s polomerom  ${polomer}  je: ${pi*(polomer^2)}`);
 
-// console.log(` Obvod obdlznika so stranami ${stranaA} a ${stranaB} je: ${2*(stranaA + stranaB)}`);
+// console.log(` Obvod kruhu s polomerom ${polomer}  je: ${2*pi*polomer}`);
+
+// let vek = 15;
+// if (vek >= 18){
+//   // kod ktorý sa vykoná, ak je podmienka splnená (TRUE)
+//   console.log("Môžeš šoférovať!")
+// }
+
+// let cenaNakupu = 101;
+// if (cenaNakupu > 100){
+//   console.log("Doprava zdarma!");
+// }
+
+let cislo = 0;
+
+if (cislo < 0 ){
+  console.log(`Cislo ${cislo} je zaporne`);
+}
+else{
+  console.log(`Cislo ${cislo} je kladne alebo nula`);
+}
