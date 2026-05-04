977acccf60801acc34553b4622daf16d3456e2ac
Jozef Dubec <127235676+srandykopec@users.noreply.github.com>
2026-05-04 12:20:43 +0200

    opakovanie  premenne

---
+++ EPU/09_podmienky/script.js
@@ -0,0 +1,19 @@
+// Obvod a obsah stvorca
+
+// let stranaA = 4;
+// let stranaB = 6;
+
+// console.log(` Obsah obdlznika so stranami  ${stranaA} a ${stranaB} je: ${stranaA * stranaB}`);
+
+// console.log(` Obvod obdlznika so stranami ${stranaA} a ${stranaB} je: ${2*(stranaA + stranaB)}`);
+
+//
+// Obvod a obsah kruhu
+//
+
+let polomer = 4;
+let pi = 3.14;
+
+console.log(` Obsah kruhu s polomerom  ${polomer}  je: ${pi*(polomer^2)}`);
+
+// console.log(` Obvod obdlznika so stranami ${stranaA} a ${stranaB} je: ${2*(stranaA + stranaB)}`);
