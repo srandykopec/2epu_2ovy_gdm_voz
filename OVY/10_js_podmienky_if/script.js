cbbaa1cdb7bc1cfbe7ec444bc2649c5b2a0151da
Jozef Dubec <jozef.vyucba@gmail.com>
2026-05-06 12:40:45 +0200

    cvičenia if

---
+++ OVY/10_js_podmienky_if/script.js
@@ -0,0 +1,48 @@
+
+//zistime, či je číslo kladné
+
+// let cislo = 4;
+
+// if ( cislo > 0 ) {
+//   console.log(`Číslo ${cislo} je kladné`);
+//   // tento kód sa vykoná iba ak je podmienka true
+// };
+
+//*************************************************** 
+// si plnoletý
+//*************************************************** 
+
+
+// let vek = 9;
+
+// if (vek >= 18){
+//   console.log("Si plnoletý!");
+// } else {
+//   console.log("Nie si plnoletý!");
+// }
+
+// let siPlnolety = true;
+
+// if ( siPlnolety ) {   //alternatíva (siPlnolety === true)
+//   console.log("Si plnoletý!");
+// } else {
+//   console.log("Nie si plnoletý!");
+// }
+
+//*************************************************** 
+// kontrola hesla
+//*************************************************** 
+
+let hesloInput = "nku123";
+let hesloDatabaza = "nku123x";
+
+if (hesloInput === hesloDatabaza){
+  console.log("Si prihlásený");
+} else {
+  console.log("Nesprávne heslo.");
+}
+
+
+Vyhral hrac2 so skóre 85!
+
+
